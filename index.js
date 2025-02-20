90c5c7555e33da325f41cdde858a8ba7916f2b88
ashrafulhossain1 <ahmeadashraful@gmail.com>
2025-02-21 01:58:53 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+// mid
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.jkfsd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+  deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+
+    const userCollection = client.db("taskAppDB").collection("users");
+
+    // Create a new user
+
+    // get users
+    app.get('/users', async (req, res) => {
+      const cursor = userCollection.find({});
+      const users = await cursor.toArray();
+      res.send(users);
+    })
+
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+  res.send("assignment Job task server is running...");
+})
+app.listen(port, () => {
+  console.log(`server running on PORT: ${port}`);
+})
